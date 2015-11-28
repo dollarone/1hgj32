7742fe42e3fbe752381a669e9e9fe29a73a0a542
Manuel Calavera <various.artists@yahoo.com>
2015-11-28 22:51:12 +0000

    bugfixes

--- js/Game.js
+++ js/Game.js
@@ -10,7 +10,6 @@ PlatformerGame.Game.prototype = {
     //  We're going to be using physics, so enable the Arcade Physics system
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-//    this.game.world.setBounds(0, 0, 1260, 14000);
 
 
     this.map = this.game.add.tilemap('level');
@@ -18,24 +17,26 @@ PlatformerGame.Game.prototype = {
     this.blockedLayer = this.map.createLayer('blockedLayer', 1260, 14000);
     this.blockedLayer.fixedToCamera = false;
     this.blockedLayer.resizeWorld();
+    this.game.world.setBounds(0, 0, 1260, 14000);
     //this.blockedLayer.debug = true;
 
-    
-    this.map.setCollisionBetween(1, 2000, true, 'blockedLayer');
-    
     this.createItems();
+    
+    this.map.setCollisionBetween(1, 5000, true, 'blockedLayer');
+    
 
     // The this.player and its settings
-    //var result = this.findObjectsByType('playerStart', this.map, 'objectsLayer');
-    //this.player = this.game.add.sprite(result[0].x, result[0].y - 144,  'dude'); 
-    this.player = this.game.add.sprite(155,155,  'dude'); 
+    var result = this.findObjectsByType('playerStart', this.map, 'objectsLayer');
+    this.player = this.game.add.sprite(result[0].x, result[0].y,  'dude'); 
+    
 
     //  We need to enable physics on the this.player
-    this.game.physics.arcade.enable(this.player);
+    
     
 
 
     this.player.enableBody = true;
+    this.game.physics.arcade.enable(this.player);
     //  this.player physics properties. Give the little guy a slight bounce.
     this.player.body.bounce.y = 0.2;
     this.player.body.gravity.y = 600;
@@ -48,15 +49,17 @@ PlatformerGame.Game.prototype = {
 
     //  Our controls.
     this.cursors = this.game.input.keyboard.createCursorKeys();
-    //this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_LOCKON);
+    
     this.game.camera.follow(this.player);
-    //this.this.player.fixedToCamera = true;
+    
     this.infoText = this.game.add.text(600, 300, 'You died!', { fontSize: '32px', fill: '#fff'});
-        this.infoText.fixedToCamera = true;
-        this.infoText.visible = false;
-        this.winText = this.game.add.text(600, 300, 'You have escaped!', { fontSize: '32px', fill: '#fff'});
-        this.winText.fixedToCamera = true;
-        this.winText.visible = false;
+    this.infoText.fixedToCamera = true;
+    this.infoText.visible = false;
+
+    this.winText = this.game.add.text(600, 300, 'You have escaped!', { fontSize: '32px', fill: '#fff'});
+    this.winText.fixedToCamera = true;
+    this.winText.visible = false;
+
     this.win = false;
     this.death = false;
     this.counter = 0;
@@ -64,21 +67,16 @@ PlatformerGame.Game.prototype = {
 
   update: function() {
 
-    if (this.player.body.velocity.y > 500) {
-        this.player.body.velocity.y = 750;
-
-    }
-
     if (this.death) {
         this.infoText.visible = true;
         this.counter--;
-        if (this.counter === 0) {
-            this.state.start('Game');
+        if (this.counter < 0) {
+            this.state.restart();
         }
     }
     if (this.win) {
         this.counter--;
-        if (this.counter === 0) {
+        if (this.counter < 0) {
             this.winText.visible = true;
             this.game.paused = true;
         }
@@ -87,34 +85,32 @@ PlatformerGame.Game.prototype = {
 
     //  Collide the this.player and the stars with the platforms
     this.game.physics.arcade.collide(this.player, this.blockedLayer);
+    this.game.physics.arcade.collide(this.items, this.blockedLayer);
 
-    //  Checks to see if the this.player overlaps with any of the stars, if he does call the collectStar function
     this.game.physics.arcade.overlap(this.player, this.items, this.itemCollision, null, this);
 
     //  Reset the this.players velocity (movement)
     this.player.body.velocity.x = 0;
+    if (this.player.body.velocity.y > 500) {
+        this.player.body.velocity.y = 500;
+    }
 
     if (this.cursors.left.isDown)
     {
         //  Move to the left
         this.player.body.velocity.x = -300;
-
-
         this.player.animations.play('left');
     }
     else if (this.cursors.right.isDown)
     {
         //  Move to the right
         this.player.body.velocity.x = 300;
-        //this.game.camera.x += 20;
-
         this.player.animations.play('right');
     }
     else
     {
         //  Stand still
         this.player.animations.stop();
-
         this.player.frame = 4;
     }
 
@@ -122,7 +118,6 @@ PlatformerGame.Game.prototype = {
   },
       createItems: function() {
         this.items = this.game.add.group();
-        this.items.enableBody = true;
         
         result = this.findObjectsByType('item', this.map, 'objectsLayer');
         result.forEach(function(element) {
@@ -146,6 +141,8 @@ PlatformerGame.Game.prototype = {
     createFromTiledObject: function(element, group) {
         var sprite = group.create(element.x, element.y, 'objects');
         sprite.frame = parseInt(element.properties.frame);
+        sprite.enableBody = true;
+        sprite.fixedToCamera = false;
 
         // copy all of the sprite's properties
         Object.keys(element.properties).forEach(function(key) {
@@ -164,7 +161,6 @@ PlatformerGame.Game.prototype = {
                 this.counter = 3;
                 break;
         }
-        
-
+    
     },
 };
